1ec43a90e932eb54a8aa89babdf4299e39f3447d
Manish <manishkumar9540898@gmail.com>
2026-01-02 09:00:30 +0530

    chore: add tailwindcss utility class & configure basepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/flux-drive",
+  basePath: "/flex-drive",
 };
 
 export default nextConfig;
